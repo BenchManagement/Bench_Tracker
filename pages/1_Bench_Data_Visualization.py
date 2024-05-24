8ef0d2746fc8055e36d588fc61e3446e4b8c565b
RavitejaRuthala <ruthalaraviteja@gmail.com>
2024-05-24 13:20:52 -0400

    Changed the logic for counting the Bench removal dates and also fixed the issues for redudant values on the plots.

--- pages/1_Bench_Data_Visualization.py
+++ pages/1_Bench_Data_Visualization.py
@@ -2,8 +2,10 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+from datetime import datetime
 
 timeCount=-1
+removal_date_count=0
 employeeRemovalDateList=[]
 regionBench = []
 regionRate = []
@@ -16,6 +18,7 @@ benchTimeAmber=[]
 benchEmployeeAmber=[]
 benchTimeBlue=[]
 benchEmployeeBlue=[]
+trimmed_designation=[]
 
 st.set_page_config(page_title="Bench Data Visualization", page_icon="📈")
 st.markdown("# Bench Data Visualization")
@@ -28,15 +31,17 @@ if(benchData):
     empNames = rawData['Employee_LName_FName']
     dCategory = rawData['Activity_Category']
     dLevel = rawData['Level']
-    dDesgination = rawData['Job_Title']
+    dDesignation = rawData['Job_Title']
     regions = fixedData['Region']
     regionTotal = list(fixedData['Region_Total'])
     conSummary = st.container(border=True)
     for i in list(rawData['Bench_Removal_Start_Date']):
         if isinstance(i,str):
             removalDate+=i
+        elif isinstance(i,datetime):
+            removal_date_count = removal_date_count+1
     conSummary.write("**Total number of employees on Bench** - "+str(empNames.index.size))
-    conSummary.write('**Total number of employees with Bench Removal Date** - '+str((empNames.index.size)-removalDate.count('tentative')-removalDate.count(expected)-removalDate.count(altExpected)))
+    conSummary.write('**Total number of employees with Bench Removal Date** - '+str(removal_date_count))
     conSummary.write('**Total number of employees with Tentative Bench Removal Date** - '+ str(removalDate.count('tentative')))
     conSummary.write('**Total number of employees who needs a Bench Removal Date (TBD)** - '+ str(removalDate.count(expected)+ removalDate.count(altExpected)))
     for r in regions:
@@ -51,11 +56,11 @@ if(benchData):
             go.Bar(x=regions, y=regionRate, name="Employee % on Bench", text=regionRate)])
     regionBar.update_layout(title_text="Regional Bench Distribution: Region-Wide Percentage.", bargap=0.2, bargroupgap=0)
     st.plotly_chart(regionBar)
-    designationPie = px.histogram(dDesgination, x = "Job_Title", title='Bench Count based on Designations.', text_auto=True).update_xaxes(categoryorder='total descending')
+    designationPie = px.histogram(dDesignation.str.strip(), x="Job_Title", title='Bench Count based on Designations.', text_auto=True).update_xaxes(categoryorder='total descending')
     st.plotly_chart(designationPie)
-    levelPie = px.histogram(dLevel, x = "Level", title='Bench Count based on Experience Level.', text_auto=True).update_xaxes(categoryorder='total descending')
+    levelPie = px.histogram(dLevel.str.strip(), x = "Level", title='Bench Count based on Experience Level.', text_auto=True).update_xaxes(categoryorder='total descending')
     st.plotly_chart(levelPie)
-    categoryBar = px.histogram(dCategory, x = "Activity_Category", title='Bench Productivity Chart: Activity-Based Distribution.', text_auto=True).update_xaxes(categoryorder='total descending')
+    categoryBar = px.histogram(dCategory.str.strip(), x = "Activity_Category", title='Bench Productivity Chart: Activity-Based Distribution.', text_auto=True).update_xaxes(categoryorder='total descending')
     st.plotly_chart(categoryBar)
     for n in list(rawData['Time_On_Bench']):
         timeCount=timeCount+1
