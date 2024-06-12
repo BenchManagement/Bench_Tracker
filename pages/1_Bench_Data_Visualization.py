f7c95bccff73a47f6e8beee635415b1275a1e9c5
RavitejaRuthala <ruthalaraviteja@gmail.com>
2024-06-12 13:51:15 -0400

    Updated code as per PEP8.

--- pages/1_Bench_Data_Visualization.py
+++ pages/1_Bench_Data_Visualization.py
@@ -4,20 +4,20 @@ import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime
 
-timeCount=-1
-removal_date_count=0
-employeeRemovalDateList=[]
+timeCount = -1
+removal_date_count = 0
+employeeRemovalDateList = []
 regionBench = []
 regionRate = []
 removalDate = ""
 expected = 'TBD'
 altExpected = 'TDB'
-benchTimeRed=[]
-benchEmployeeRed=[]
-benchTimeAmber=[]
-benchEmployeeAmber=[]
-benchTimeBlue=[]
-benchEmployeeBlue=[]
+benchTimeRed = []
+benchEmployeeRed = []
+benchTimeAmber = []
+benchEmployeeAmber = []
+benchTimeBlue = []
+benchEmployeeBlue = []
 
 st.set_page_config(page_title="Bench Data Visualization", page_icon="📈")
 st.markdown("""
@@ -30,10 +30,10 @@ footer {visibility: hidden;}
 st.markdown("# Bench Data Visualization")
 st.sidebar.header("Bench Data Visualization")
 
-benchData = st.file_uploader("Choose the data file")
-if(benchData):
-    rawData = pd.read_excel(benchData, sheet_name="Bench")
-    fixedData = pd.read_excel(benchData, sheet_name="Region_Count")
+bench_data = st.file_uploader("Choose the data file")
+if bench_data:
+    rawData = pd.read_excel(bench_data, sheet_name="Bench")
+    fixedData = pd.read_excel(bench_data, sheet_name="Region_Count")
     empNames = rawData['Employee_LName_FName']
     dCategory = rawData['Activity_Category']
     dLevel = rawData['Level']
@@ -42,46 +42,58 @@ if(benchData):
     regionTotal = list(fixedData['Region_Total'])
     conSummary = st.container(border=True)
     for i in list(rawData['Bench_Removal_Start_Date']):
-        if isinstance(i,str):
-            removalDate+=i
-        elif isinstance(i,datetime):
-            removal_date_count = removal_date_count+1
-    conSummary.write("**Total number of employees on Bench** - "+str(empNames.index.size))
-    conSummary.write('**Total number of employees with Bench Removal Date** - '+str(removal_date_count))
-    conSummary.write('**Total number of employees with Tentative Bench Removal Date** - '+ str(removalDate.count('tentative')))
-    conSummary.write('**Total number of employees who needs a Bench Removal Date (TBD)** - '+ str(removalDate.count(expected)+ removalDate.count(altExpected)))
+        if isinstance(i, str):
+            removalDate += i
+        elif isinstance(i, datetime):
+            removal_date_count = removal_date_count + 1
+    conSummary.write("**Total number of employees on Bench** - " + str(empNames.index.size))
+    conSummary.write('**Total number of employees with Bench Removal Date** - ' + str(removal_date_count))
+    conSummary.write(
+        '**Total number of employees with Tentative Bench Removal Date** - ' + str(removalDate.count('tentative')))
+    conSummary.write('**Total number of employees who needs a Bench Removal Date (TBD)** - ' + str(
+        removalDate.count(expected) + removalDate.count(altExpected)))
     for r in regions:
         regionBench.append(list(rawData['Region']).count(r))
     for i in range(len(regionBench)):
-        regionRate.append(round((regionBench[i]*100)/regionTotal[i],2))
-    regionPie = px.pie(rawData['Region'], values=regionBench, names=regions, title='Regional Bench Distribution: PLATO-Wide Percentage.')
+        regionRate.append(round((regionBench[i] * 100) / regionTotal[i], 2))
+    regionPie = px.pie(rawData['Region'], values=regionBench, names=regions, title='Regional Bench Distribution: '
+                                                                                   'PLATO-Wide Percentage.')
     st.plotly_chart(regionPie)
     region_plot = go.Figure(
         data=[go.Bar(x=regions, y=regionTotal, name="Total region count", text=regionTotal),
-            go.Bar(x=regions, y=regionBench, name="Bench count per region", text=regionBench),
-            go.Bar(x=regions, y=regionRate, name="Employee % on Bench", text=regionRate)])
-    region_plot.update_layout(title_text="Regional Bench Distribution: Region-Wide Percentage.", bargap=0.2, bargroupgap=0)
+              go.Bar(x=regions, y=regionBench, name="Bench count per region", text=regionBench),
+              go.Bar(x=regions, y=regionRate, name="Employee % on Bench", text=regionRate)])
+    region_plot.update_layout(title_text="Regional Bench Distribution: Region-Wide Percentage.", bargap=0.2,
+                              bargroupgap=0)
     st.plotly_chart(region_plot)
-    designation_plot = px.histogram(dDesignation.str.strip(), x="Job_Title", title='Bench Count based on Designations.', text_auto=True).update_xaxes(categoryorder='total descending')
+    designation_plot = px.histogram(dDesignation.str.strip(), x="Job_Title", title='Bench Count based on Designations.',
+                                    text_auto=True).update_xaxes(categoryorder='total descending')
     st.plotly_chart(designation_plot)
-    level_plot = px.histogram(dLevel.str.strip(), x = "Level", title='Bench Count based on Experience Level.', text_auto=True).update_xaxes(categoryorder='total descending')
+    level_plot = px.histogram(dLevel.str.strip(), x="Level", title='Bench Count based on Experience Level.',
+                              text_auto=True).update_xaxes(categoryorder='total descending')
     st.plotly_chart(level_plot)
-    category_plot = px.histogram(dCategory.str.strip(), x = "Activity_Category", title='Bench Productivity Chart: Activity-Based Distribution.', text_auto=True).update_xaxes(categoryorder='total descending')
+    category_plot = px.histogram(dCategory.str.strip(), x="Activity_Category", title='Bench Productivity Chart: '
+                                                                                     'Activity-Based '
+                                                                                     'Distribution.',
+                                 text_auto=True).update_xaxes(categoryorder='total descending')
     st.plotly_chart(category_plot)
     for n in list(rawData['Time_On_Bench']):
-        timeCount=timeCount+1
-        if (n < 45):
+        timeCount = timeCount + 1
+        if n < 45:
             benchEmployeeBlue.append(rawData['Employee_LName_FName'][timeCount])
             benchTimeBlue.append(n)
-        elif (45 <= n <= 90):
+        elif 45 <= n <= 90:
             benchEmployeeAmber.append(rawData['Employee_LName_FName'][timeCount])
             benchTimeAmber.append(n)
-        elif (n > 90):
+        elif n > 90:
             benchEmployeeRed.append(rawData['Employee_LName_FName'][timeCount])
             benchTimeRed.append(n)
     benchTimeBar = go.Figure(
-        data=[go.Bar(x=benchEmployeeRed, y=benchTimeRed, name="Tenure more than 90 days", text=benchTimeRed, marker_color='FireBrick'),
-            go.Bar(x=benchEmployeeAmber, y=benchTimeAmber, name="Tenure between 45 to 90 days", text=benchTimeAmber, marker_color='DarkOrange'),
-            go.Bar(x=benchEmployeeBlue, y=benchTimeBlue, name="Tenure less than 45 days", text=benchTimeBlue, marker_color='Blue')])
+        data=[go.Bar(x=benchEmployeeRed, y=benchTimeRed, name="Tenure more than 90 days", text=benchTimeRed,
+                     marker_color='FireBrick'),
+              go.Bar(x=benchEmployeeAmber, y=benchTimeAmber, name="Tenure between 45 to 90 days", text=benchTimeAmber,
+                     marker_color='DarkOrange'),
+              go.Bar(x=benchEmployeeBlue, y=benchTimeBlue, name="Tenure less than 45 days", text=benchTimeBlue,
+                     marker_color='Blue')])
     benchTimeBar.update_layout(title_text="Bench Tenure Summary.", bargap=0.2, bargroupgap=0)
     st.plotly_chart(benchTimeBar)
--- pages/2_Bench_Details.py
+++ pages/2_Bench_Details.py
@@ -2,14 +2,14 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 
-managerList=[]
-employeeList=[]
-employeeRemovalDateList=[]
-count=-1
-removalDateCount=-1
-email=""
-employee=""
-employeeRemovalDate=""
+managerList = []
+employeeList = []
+employeeRemovalDateList = []
+count = -1
+removalDateCount = -1
+email = ""
+employee = ""
+employeeRemovalDate = ""
 expected = 'TBD'
 altExpected = 'TDB'
 
@@ -24,9 +24,9 @@ footer {visibility: hidden;}
 st.markdown("# Bench Details")
 st.sidebar.header("Bench Details")
 
-benchData = st.file_uploader("Choose the data file")
-if(benchData):
-    rawData = pd.read_excel(benchData, sheet_name="Bench")
+bench_data = st.file_uploader("Choose the data file")
+if bench_data:
+    rawData = pd.read_excel(bench_data, sheet_name="Bench")
     empNames = rawData['Employee_LName_FName']
     activityCategories = rawData['Activity_Category'].unique()
     with st.popover("**Associates with no Bench Removal Date assigned.**"):
@@ -36,51 +36,75 @@ if(benchData):
             st.write(name)
     conValidationDate = st.container(border=True)
     conValidationDate.write("**Associate's activity End date Status Check:**")
-    validationDate = conValidationDate.date_input("This section will pull out the list of associates (along with Manager Email IDs) with 'Activity End Date' as past date from the bench report on the basis of the date entered below.", value=None)
-    if(validationDate):
+    validationDate = conValidationDate.date_input(
+        "This section will pull out the list of associates (along with Manager Email IDs) with 'Activity End Date' as "
+        "past date from the bench report on the basis of the date entered below.",
+        value=None)
+    if validationDate:
         for dateValue in rawData['Activity_End']:
-            count=count+1
+            count = count + 1
             if isinstance(dateValue, datetime):
-                if(dateValue.date() > validationDate):
+                if dateValue.date() > validationDate:
                     pass
                 else:
-                    managerList.append(rawData["Manager_ Email_Id"][count])  
+                    managerList.append(rawData["Manager_ Email_Id"][count])
                     employeeList.append(rawData['Employee_LName_FName'][count])
-        for id in list(set(managerList)):
-            email= email+str(id)+";"+" "
+        for manager_name in list(set(managerList)):
+            email = email + str(manager_name) + ";" + " "
         for name in list(set(employeeList)):
-            employee= employee+str(name)+"\n"
-        if(len(list(set(managerList)))>0):
-            conValidationDate.write("Detected "+str(len(list(set(managerList))))+" Managers. Download the below file to get their contact details.")
-            conValidationDate.download_button('Download Email IDs of Reporting Managers', data=email, file_name='Bench Report Update Required - Managers.txt', help="Provides a text file that contains Email IDs of Managers who needs to update their Associate's Bench Activity details.")
-            conValidationDate.download_button('Download Employee list', data=employee, file_name='Bench Report Update Required - Employees.txt', help="Provides a text file that contains the list of employees whose End Date needs an update.")
-            #if conValidationDate.button('Send Emails to the respective Reporting Managers'):
+            employee = employee + str(name) + "\n"
+        if len(list(set(managerList))) > 0:
+            conValidationDate.write("Detected " + str(
+                len(list(set(managerList)))) + " Managers. Download the below file to get their contact details.")
+            conValidationDate.download_button('Download Email IDs of Reporting Managers', data=email,
+                                              file_name='Bench Report Update Required - Managers.txt',
+                                              help="Provides a text file that contains Email IDs of Managers who "
+                                                   "needs to update their Associate's Bench Activity details.")
+            conValidationDate.download_button('Download Employee list', data=employee,
+                                              file_name='Bench Report Update Required - Employees.txt',
+                                              help="Provides a text file that contains the list of employees whose "
+                                                   "End Date needs an update.")
         else:
             conValidationDate.write("Bench Report is updated. No action is required.")
     conValidationRemovalDate = st.container(border=True)
-    validationRemovalDate = conValidationRemovalDate.date_input("**Associate's Bench Removal date Status Check:**", value=None, help="This section provides list of employees with 'Bench Removal Date' as past date on the basis of reference date entered.")
-    if(validationRemovalDate):
+    validationRemovalDate = conValidationRemovalDate.date_input("**Associate's Bench Removal date Status Check:**",
+                                                                value=None,
+                                                                help="This section provides list of employees with "
+                                                                     "'Bench Removal Date' as past date on the basis "
+                                                                     "of reference date entered.")
+    if validationRemovalDate:
         for dateValue in rawData['Bench_Removal_Start_Date']:
-            removalDateCount=removalDateCount+1
+            removalDateCount = removalDateCount + 1
             if isinstance(dateValue, datetime):
-                if(dateValue.date() > validationRemovalDate):
+                if dateValue.date() > validationRemovalDate:
                     pass
-                else: 
+                else:
                     employeeRemovalDateList.append(rawData['Employee_LName_FName'][removalDateCount])
         for name in employeeRemovalDateList:
-            employeeRemovalDate= employeeRemovalDate+str(name)+"\n"
-        if(len(employeeRemovalDateList)>0):
-            conValidationRemovalDate.download_button('Download Employee list who has a Removal Bench date as a past value.', data=employeeRemovalDate, file_name='Bench Removal Date - Update Required - Employees.txt', help="Provides a text file that contains the list of employees whose Bench Removal Date needs an update.")
+            employeeRemovalDate = employeeRemovalDate + str(name) + "\n"
+        if len(employeeRemovalDateList) > 0:
+            conValidationRemovalDate.download_button(
+                'Download Employee list who has a Removal Bench date as a past value.', data=employeeRemovalDate,
+                file_name='Bench Removal Date - Update Required - Employees.txt',
+                help="Provides a text file that contains the list of employees whose Bench Removal Date needs an "
+                     "update.")
         else:
-            conValidationRemovalDate.write("Bench Removal dates are up to date. No action is required.")     
+            conValidationRemovalDate.write("Bench Removal dates are up to date. No action is required.")
     conCategory = st.container(border=True)
-    selectedCat = conCategory.selectbox('**Activity Category Lookup:**', activityCategories, index=None, placeholder='Choose an activity category...', help="Select an activity to know the list of associates working in the particular area.")
-    if(selectedCat):
-        conCategory.write('Below employees are working on '+selectedCat)
-        conCategory.table(rawData.query('Activity_Category == @selectedCat')[['Employee_LName_FName', 'Activity_Details']])     
+    selectedCat = conCategory.selectbox('**Activity Category Lookup:**', activityCategories, index=None,
+                                        placeholder='Choose an activity category...',
+                                        help="Select an activity to know the list of associates working in the "
+                                             "particular area.")
+    if selectedCat:
+        conCategory.write('Below employees are working on ' + selectedCat)
+        conCategory.table(
+            rawData.query('Activity_Category == @selectedCat')[['Employee_LName_FName', 'Activity_Details']])
     conEmployee = st.container(border=True)
-    selectedEmp = conEmployee.selectbox('**Associate Task Overview:**',empNames, index=None, placeholder='Choose an associate...', help="This section provides details about the work and activities of a selected associate.")
-    if(selectedEmp):
+    selectedEmp = conEmployee.selectbox('**Associate Task Overview:**', empNames, index=None,
+                                        placeholder='Choose an associate...',
+                                        help="This section provides details about the work and activities of a "
+                                             "selected associate.")
+    if selectedEmp:
         selectedRow = rawData.query("Employee_LName_FName == @selectedEmp")
         conEmployee.write(selectedRow['Activity_Category'])
         conEmployee.write(selectedRow['Activity_Details'])
